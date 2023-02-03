3af74827f874bb6bc51ef21602791d8a38d06c5d
Gizel Liang <gizelliang@gmail.com>
2023-02-03 16:20:23 +0800

    Insert the Stimulus Controller for the Dropdown Menu

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -1,12 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
-import {enter, leave, toggle} from 'el-transition'
+import { toggle } from 'el-transition'
 export default class extends Controller {
-  static targets = ['openUserMenu'];
+  static targets = ['openUserMenu', 'userAuthLink'];
   connect() {
     // this.element.textContent = "Bye World"
     this.openUserMenuTarget.addEventListener('click', this.toggleDropdownMenu)
+    this.userAuthLinkTargets.forEach((link) => {
+      link.addEventListener('click', () => {
+        console.log("user links clicked");
+      });
+    });
   }
   toggleDropdownMenu(){
-    toggle(document.getElementById('menu-dropdown-items'))
-  }
+    toggle(document.getElementById('menu-dropdown-items'));
+    }
 }
