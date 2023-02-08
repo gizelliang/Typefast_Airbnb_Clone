08c4af8e6cdeb9efc752c3c9da1351cbef996eae
Gizel Liang <gizelliang@gmail.com>
2023-02-08 18:44:04 +0800

    Add Close Button to the Modal

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -1,18 +1,21 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus'
 import { toggle } from 'el-transition'
+
 export default class extends Controller {
   static targets = ['openUserMenu', 'userAuthLink'];
+
   connect() {
-    // this.element.textContent = "Bye World"
     this.openUserMenuTarget.addEventListener('click', this.toggleDropdownMenu)
+
     this.userAuthLinkTargets.forEach((link) => {
-      link.addEventListener('click', () => {
+      link.addEventListener('click', (e) => {
         e.preventDefault();
         document.getElementById('modal-trigger').click();
       });
     });
   }
-  toggleDropdownMenu(){
+
+  toggleDropdownMenu() {
     toggle(document.getElementById('menu-dropdown-items'));
-    }
+  }
 }
--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,25 +1,31 @@
-import { Controller } from "@hotwired/stimulus"
-import {enter, leave, toggle} from 'el-transition'
+import { Controller } from '@hotwired/stimulus'
+import { enter, leave, toggle } from 'el-transition'
+
 export default class extends Controller {
+  static targets = ['closeButton'];
+
   connect() {
-    // this.element.textContent = "Bye World"
-    enter(document.getElementById('modal-wrapper'))
-    enter(document.getElementById('modal-backdrop'))
-    enter(document.getElementById('modal-panel'))
     document.getElementById('modal-wrapper').addEventListener('click', this.closeModal);
-  }
-  closeModal(event){
-    const modalPanelClicked = document.getElementById('modal-panel').contains(event.target);
-    if (!modalPanelClicked && event.target.id !== "modal-trigger" ){
-    leave(document.getElementById('modal-wrapper'))
-    leave(document.getElementById('modal-backdrop'))
-    leave(document.getElementById('modal-panel'))
-    }
-  }
-  showModal() {
-    enter(document.getElementById('modal-wrapper'))
-    enter(document.getElementById('modal-backdrop'))
-    enter(document.getElementById('modal-panel'))
+    this.closeButtonTarget.addEventListener('click', () => {
+      leave(document.getElementById('modal-wrapper'));
+      leave(document.getElementById('modal-backdrop'));
+      leave(document.getElementById('modal-panel'));
+    });
   }
 
+  closeModal(event) {
+    const modalPanelClicked = document.getElementById('modal-panel').contains(event.target);
+
+    if (!modalPanelClicked && event.target.id !== 'modal-trigger') {
+      leave(document.getElementById('modal-wrapper'));
+      leave(document.getElementById('modal-backdrop'));
+      leave(document.getElementById('modal-panel'));
+    }
+  }
+
+  showModal() {
+    enter(document.getElementById('modal-wrapper'));
+    enter(document.getElementById('modal-backdrop'));
+    enter(document.getElementById('modal-panel'));
+  }
 }
