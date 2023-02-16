25f7fbed916d38cd1a221cb2763172b5d480b92e
Gizel Liang <gizelliang@gmail.com>
2023-02-16 15:25:17 +0800

    Change the Version of Axios & Install Axios

--- app/javascript/controllers/users_by_email_auth_controller.js
+++ app/javascript/controllers/users_by_email_auth_controller.js
@@ -1,8 +1,18 @@
-import { Controller } from '@hotwired/stimulus'
+import { Controller } from "@hotwired/stimulus";
+import axios from 'axios'
 
 export default class extends Controller {
     static targets = ['email', 'submit']
     connect(){
-       
+       console.log('axios:', axios)
+       this.submitTarget.addEventListener('click', (e) =>{
+        e.preventDefault();
+        if(this.emailTarget.value.length === 0){
+           
+        } else {
+
+        }
+       });
+
     }
 }
