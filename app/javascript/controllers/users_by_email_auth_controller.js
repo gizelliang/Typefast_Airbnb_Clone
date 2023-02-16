c14ee5ac5b378c47614d2c616614968b59a6d4d4
Gizel Liang <gizelliang@gmail.com>
2023-02-16 16:47:00 +0800

    Fix the modal template

--- app/javascript/controllers/users_by_email_auth_controller.js
+++ app/javascript/controllers/users_by_email_auth_controller.js
@@ -4,15 +4,15 @@ import axios from 'axios'
 export default class extends Controller {
     static targets = ['email', 'submit']
     connect(){
-       console.log('axios:', axios)
        this.submitTarget.addEventListener('click', (e) =>{
         e.preventDefault();
         if(this.emailTarget.value.length === 0){
-           
-        } else {
-
+            params: {
+                email: this.emailTarget.value
+            }
         }
-       });
+           else{
 
+           })
+        }
     }
-}
