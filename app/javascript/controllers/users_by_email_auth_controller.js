8a8a0e006b51c475b66711590347d3794a2b1e53
Gizel Liang <gizelliang@gmail.com>
2023-02-16 19:11:44 +0800

    Use axios and Turbo to redirect based on the user's email

--- app/javascript/controllers/users_by_email_auth_controller.js
+++ app/javascript/controllers/users_by_email_auth_controller.js
@@ -1,18 +1,29 @@
 import { Controller } from "@hotwired/stimulus";
-import axios from 'axios'
+import axios from 'axios';
 
 export default class extends Controller {
     static targets = ['email', 'submit']
     connect(){
        this.submitTarget.addEventListener('click', (e) =>{
+        console.log('axios', axios);
         e.preventDefault();
-        if(this.emailTarget.value.length === 0){
-            params: {
-                email: this.emailTarget.value
-            }
-        }
-           else{
+        if (this.emailTarget.value.length === 0){
 
-           })
-        }
+        } else {
+            axios.get('/api/users_by_email', {
+                params: {
+                    email: this.emailTarget.value
+                },
+                headers: {
+                    'ACCEPT': 'application/json'
+                },
+            }).then((response)=>{
+                Turbo.visit('/users/sign_in');
+            }).catch((response)=>{
+                Turbo.visit('/users/sign_up');
+            })
+            }
+        });
     }
+}
+
