494a802a6e5e8303e3f6cab9d17ea027a13c3bfd
Gizel Liang <gizelliang@gmail.com>
2023-02-20 15:53:21 +0800

    Create a property model & install shoulda matcher for rspec

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_31_035734) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_20_072349) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "properties", force: :cascade do |t|
+    t.string "name"
+    t.string "headline"
+    t.text "description"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
---
+++ spec/models/property_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Property, type: :model do
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:headline) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:city) }
+  it { should validate_presence_of(:country) }
+  it { should validate_presence_of(:state) }
+end
