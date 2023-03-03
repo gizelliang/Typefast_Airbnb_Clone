edc75e7fa0a8585f7f4ac661e846491798647b8e
Gizel Liang <gizelliang@gmail.com>
2023-03-03 13:00:10 +0800

    Pin geolib through importmap

--- app/javascript/controllers/geolocation_controller.js
+++ app/javascript/controllers/geolocation_controller.js
@@ -1,11 +1,18 @@
-import { Controller } from '@hotwired/stimulus'
+import { Controller } from '@hotwired/stimulus';
+import { getDistance } from 'geolib';
 
 export default class extends Controller {
     
     connect() {
+        console.log('geolib: ', getDistance);
         window.navigator.geolocation.getCurrentPosition((position) => {
             this.element.dataset.latitude = position.coords.latitude;
             this.element.dataset.longitude = position.coords.longitude;
+            const distance = getDistance(
+              { latitude: position.coords.latitude, longitude: position.coords.longitude},
+              { latitude: 47.620146, longitude: -100.540737 }
+            );
+            console.log('distance: ', distance);
           });
   }
 }
