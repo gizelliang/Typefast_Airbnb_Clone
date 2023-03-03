5402dffd470f06672a7abd6fa2b0ad0423fc869d
Gizel Liang <gizelliang@gmail.com>
2023-03-03 15:57:49 +0800

    Use getDistance in geolib to calculate the distance

--- app/javascript/controllers/geolocation_controller.js
+++ app/javascript/controllers/geolocation_controller.js
@@ -1,10 +1,10 @@
 import { Controller } from '@hotwired/stimulus';
-import { getDistance } from 'geolib';
+import { getDistance, convertDistance } from 'geolib';
 
 export default class extends Controller {
-    
+    static targets = ['property']
     connect() {
-        console.log('geolib: ', getDistance);
+        console.log(this.propertyTargets);
         window.navigator.geolocation.getCurrentPosition((position) => {
             this.element.dataset.latitude = position.coords.latitude;
             this.element.dataset.longitude = position.coords.longitude;
@@ -12,7 +12,14 @@ export default class extends Controller {
               { latitude: position.coords.latitude, longitude: position.coords.longitude},
               { latitude: 47.620146, longitude: -100.540737 }
             );
-            console.log('distance: ', distance);
+            console.log(convertDistance(distance, 'km'));
+            this.propertyTargets.forEach((propertyTarget) => {
+              let distanceFrom = getDistance(
+                { latitude: position.coords.latitude, longitude: position.coords.longitude},
+                { latitude: propertyTarget.dataset.latitude, longitude: propertyTarget.dataset.longitude}, 
+                ); 
+              propertyTarget.querySelector('[data-distance-away]').innerHTML = `${Math.round(convertDistance(distanceFrom, 'km'))} kilometers away` ;
+            });
           });
   }
 }
